9d0dd8980deddfb4102d2d2cbfa83b3d6bae0f22
Aadhi Senthil Kumar <achukutty1202@gmail.com>
2025-08-30 13:04:00 +1000

    making starter code for trainee app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+//  /src
+//   /components
+//     Navbar.jsx
+//     Footer.jsx
+//     ProjectCard.jsx
+//   /pages
+//     Home.jsx
+//     About.jsx
+//     Projects.jsx
+//     Contact.jsx
+//  App.jsx
+//  index.js
+}
