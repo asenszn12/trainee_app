96bf216ef5e115742c7db61d41849d5a27fe59e8
Aadhi Senthil Kumar <achukutty1202@gmail.com>
2025-08-30 13:21:43 +1000

    understanding how resolve and alias works

--- trainee-app/vite.config.js
+++ trainee-app/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-import path from  "-path"
+import path from  "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   // use src as a starting directory to other files in different directions
-  
+  // resolve is to find the important stuff and alias is to rename it
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
