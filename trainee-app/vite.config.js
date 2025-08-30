c508c8cbf41a06e3f596fa0d87fc4e8e4ce25ec4
Aadhi Senthil Kumar <achukutty1202@gmail.com>
2025-08-30 13:14:52 +1000

    make src the starting directory path when we import something in a different directory

--- trainee-app/vite.config.js
+++ trainee-app/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
+import path from  "-path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // use src as a starting directory to other files in different directions
+  
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 })
