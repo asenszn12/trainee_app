93bffd5e818386eb41ef42f599e53a151549fdac
Aadhi Senthil Kumar <achukutty1202@gmail.com>
2025-09-07 11:29:05 +1000

    making foundation for hero section

--- trainee-app/src/components/Hero.jsx
+++ trainee-app/src/components/Hero.jsx
@@ -1,6 +1,20 @@
 
 export const HeroSection = () => {
-    return <section>
+    return <section id="hero" 
+    className="relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+        <div className="container max-w-4xl mx-auto text-center z-10">
+            <div className="space-y-6">
+                <h1 className="text-4xl">
+                    <span>Hi, I'm</span>
+                    <span> Aadhi </span>
+                    <span> Senthil Kumar</span>
+                </h1>
+
+            </div>
+
+        </div>
+
 
     </section>
 }
