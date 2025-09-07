095b3beb6680bd42f81c8752b7a7d46db07af951
Aadhi Senthil Kumar <achukutty1202@gmail.com>
2025-09-07 11:47:32 +1000

    added transitions to text and a little short summary of what my portfolio entails

--- trainee-app/src/components/Hero.jsx
+++ trainee-app/src/components/Hero.jsx
@@ -5,12 +5,19 @@ export const HeroSection = () => {
     >
         <div className="container max-w-4xl mx-auto text-center z-10">
             <div className="space-y-6">
-                <h1 className="text-4xl">
-                    <span>Hi, I'm</span>
-                    <span> Aadhi </span>
-                    <span> Senthil Kumar</span>
+                <h1 className="text-4xl md:text 6xl font-bold tracking-tight">
+                    <span className="opacity-0 animate-fade-in">Hi, I'm</span>
+                    <span className="text-primary opacity-0 animate-fade-in-delay-1"> Aadhi </span>
+                    <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Senthil Kumar</span>
                 </h1>
+                <p>
+                    Welcome to my Zzzone. <br>
+                    </br>
+                    I hope you get to know more about me in this portfolio 
+                    and encourage you on why I'd be a <i><b>dream</b></i> fit in 
+                    your <br></br> award-winning trainee program 🫡
 
+                </p>
             </div>
 
         </div>
