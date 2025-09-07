dad07b6279b87442c8780b9bc1f1d15d3758e9e9
Aadhi Senthil Kumar <achukutty1202@gmail.com>
2025-09-07 12:05:14 +1000

    added find out more button to link to About me section

--- trainee-app/src/components/Hero.jsx
+++ trainee-app/src/components/Hero.jsx
@@ -10,14 +10,18 @@ export const HeroSection = () => {
                     <span className="text-primary opacity-0 animate-fade-in-delay-1"> Aadhi </span>
                     <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Senthil Kumar</span>
                 </h1>
-                <p>
+                <p className="text-lg md:text-xl mx-auto opacity-0 animate-fade-in-delay-4">
                     Welcome to my Zzzone. <br>
                     </br>
                     I hope you get to know more about me in this portfolio 
                     and encourage you on why I'd be a <i><b>dream</b></i> fit in 
-                    your <br></br> award-winning trainee program 🫡
-
+                    your award-winning trainee program 🫡
                 </p>
+                <div>
+                    <a href="#about" className="dreamy-button">
+                    Find out more
+                    </a>
+                </div>
             </div>
 
         </div>
