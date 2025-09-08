d2023c64362df52bcf8262c5970fcdecc33aab3a
Aadhi Senthil Kumar <achukutty1202@gmail.com>
2025-09-08 19:07:48 +1000

    removed uncessary modifications to vite.config, which made everything not work

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import path from  "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/trainee_app/',
 
   // use src as a starting directory to other files in different directions
   // resolve is to find the important stuff and alias is to rename it
